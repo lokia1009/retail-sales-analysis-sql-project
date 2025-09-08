d59beb04bb8311c6ee9cc7f8998a42ed8879b592
Lokeshwaran A <92937215+lokia1009@users.noreply.github.com>
2025-09-09 00:44:17 +0530

    Update retail_sales_analysis_project_01.sql

--- retail_sales_analysis_project_01.sql
+++ retail_sales_analysis_project_01.sql
@@ -207,16 +207,23 @@ GROUP BY
 
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
 
-SELECT
-	CASE
-		WHEN HOUR(sale_time) <=12 THEN 'Morning'
-		WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-		ELSE 'Evening'
-	END AS shift,
-	count(customer_id)  AS 'count'
-FROM
-	retail_sales
-GROUP BY
+WITH hourly_sale
+AS
+(
+SELECT *,
+    CASE
+        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END as shift
+FROM retail_sales
+)
+SELECT 
+    shift,
+    COUNT(*) as total_orders    
+FROM 
+	hourly_sale
+GROUP BY 
 	shift;
 
 
